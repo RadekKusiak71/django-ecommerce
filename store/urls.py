102fd0ec6403f51a4e2105fff93a0b96e0963173
RadekKusiak71 <radoslawkusiak12@gmail.com>
2024-01-24 10:39:46 +0100

    ui for products page + basic view

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, ProductsPage
 
 urlpatterns = [
     path('', HomePage.as_view(), name='store-home'),
+    path('products/', ProductsPage.as_view(), name='products')
 ]
--- store/views.py
+++ store/views.py
@@ -7,3 +7,14 @@ from .models import Product
 class HomePage(ListView):
     template_name = 'home.html'
     queryset = Product.objects.all().order_by('-sales_count')[0:10]
+
+
+class ProductsPage(ListView):
+    template_name = 'products.html'
+    queryset = Product.objects.all()
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        max_price = Product.objects.all().order_by('-price').first()
+        context['max_price'] = max_price.price
+        return context
