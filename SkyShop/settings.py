98654b66aaefdab65d4f06a7e909f352a43fdb34
RadekKusiak71 <radoslawkusiak12@gmail.com>
2024-02-04 20:54:35 +0100

    gitignore update + setting .env secretkey upload

--- SkyShop/settings.py
+++ SkyShop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 '''
 
 from pathlib import Path
+from dotenv import dotenv_values
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bdhka4flwp@geblv#oq8m%jd9-6#m9&hd&(b@gqxca9-lu^=vc'
+config = dotenv_values(".env")
+SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
